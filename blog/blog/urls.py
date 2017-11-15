2b78ea477f86a86cc708dc076d1a580c87b2e804
t87186 <t87186@gmail.com>
2017-11-15 15:32:01 +0800

    11/15

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^article/', include('article.urls', namespace='article')),
     url(r'^main/', include('main.urls', namespace='main')),
     url(r'^.*', include('main.urls')),
 ]
