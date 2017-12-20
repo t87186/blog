084cbe1e1173b06c1a3da6a45f417e3fbc757834
t87186 <t87186@gmail.com>
2017-12-20 15:33:48 +0800

    7

---
+++ blog/main/views.py
@@ -0,0 +1,21 @@
+import datetime
+import random
+
+from django.shortcuts import render
+
+
+
+def main(request):
+    '''
+    Render the main page
+    '''
+    now=datetime.datetime.now()
+    context = {'like':'Django 很棒', 'now':now}
+    return render(request, 'main/main.html', context)
+
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
